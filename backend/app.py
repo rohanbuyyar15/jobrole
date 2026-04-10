eb2696d23db03f4cfb418599d9a563824f7a9605
rohanbuyyar15 <ssrohanbuyyar@gmail.com>
2026-04-11 00:35:24 +0530

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -166,4 +166,4 @@ def analyze():
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port, debug=True))
+    app.run(host='0.0.0.0', port=port, debug=True)  # Ensure only one ')' here
